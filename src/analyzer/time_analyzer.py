37bb090d0ce85a09d688281c93526c1e01a24c79
Tomáš Valenta <tomas.valenta@statsperform.com>
2023-03-14 16:24:04 +0100

    feat: New file, time analyze start

---
+++ src/analyzer/time_analyzer.py
@@ -0,0 +1,8 @@
+"""
+This module will provide classes or/and functions to provide trend analysis and predictions.
+It only works with cleaned data, max date_indexing will be applied.
+"""
+
+
+class TimeSeriesAnalyzer:
+    pass
